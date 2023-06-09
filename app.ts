6e8f550f2d8b4b6454fd25d589f5e2106dbf505c
sumit kumar <sumit1523@gmail.com>
2023-06-10 04:04:31 +0530

    number and Array type

---
+++ app.ts
@@ -0,0 +1,19 @@
+// let num1 = 4;
+
+// const calculate = (num1: number, num2: number) => {
+//   return num1 + num2;
+// };
+
+// console.log(calculate(num1, 3));
+
+// ------------------------------
+
+// const getTotal = (numbers: Array<number>) => {
+//   return numbers.reduce((acc, item) => {
+//     return acc + item;
+//   }, 0);
+// };
+
+// console.log(getTotal([3, 2, 1]));
+
+// ------------------------------
